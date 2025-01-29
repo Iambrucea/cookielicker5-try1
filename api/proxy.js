c12698c61866769b0b70ce464742c68d5b21f9d8
NathanBrodrick <163065342+Iambrucea@users.noreply.github.com>
2025-01-29 15:24:47 -0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://cookie-clicker.io/cookie-clicker-4";//your website url
+  let target = "https://slope-game.github.io/agent-walker-vs-skibidi-toilets/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
