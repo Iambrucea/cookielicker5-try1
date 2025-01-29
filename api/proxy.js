aefdc8ee850da523ce5b5513349c35bf63f7d765
NathanBrodrick <163065342+Iambrucea@users.noreply.github.com>
2025-01-29 15:12:14 -0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://swordmasters.io";//your website url
+  let target = "https://cookie-clicker2.com/cookie-clicker-4.embed";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
