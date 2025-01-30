42003a1a0502fccc2d5fb290eb54fd5d49a05710
NathanBrodrick <163065342+Iambrucea@users.noreply.github.com>
2025-01-29 19:54:38 -0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://slope-game.github.io/agent-walker-vs-skibidi-toilets/";//your website url
+  let target = "https://diep.io/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
